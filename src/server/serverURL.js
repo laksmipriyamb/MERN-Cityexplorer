dc104da229878674fd6fedd3b642fd293588899d
lakshmipriyamb <lakshmipriyamb2002@gmail.com>
2026-02-15 11:34:14 +0530

    final server link updated

--- src/server/serverURL.js
+++ src/server/serverURL.js
@@ -1,4 +1,5 @@
 // const serverURL = "https://mern-cityexplorer-server.onrender.com"
-const serverURL = "http://localhost:3000"
+// const serverURL = "http://localhost:3000"
+const serverURL = "https://mern-cityexplorer-server-1.onrender.com"
 
 export default serverURL
